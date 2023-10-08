a791359f037d2140dc836f0ab1415a04796b74de
Chenhang Cui <92932169+gzcch@users.noreply.github.com>
2023-10-08 20:25:17 +0800

    Update out_rewrite.py

--- out_rewrite.py
+++ out_rewrite.py
@@ -23,8 +23,11 @@ from tqdm import tqdm
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Demo")
-    parser.add_argument("--cfg-path", default="/nas-alinlp/yiyang.zyy/MiniGPT-4/eval_configs/minigpt4_eval.yaml", help="path to configuration file.")
+    parser.add_argument("--cfg-path", default="/eval_configs/minigpt4_eval.yaml", help="path to configuration file.")
     parser.add_argument("--gpu-id", type=int, default=0, help="specify the gpu to load the model.")
+    parser.add_argument("--input_caption", help="path to input caption.")
+    parser.add_argument("--input_image", help="path to image file.")
+    parser.add_argument("--output_file", help="path to output file.")
     parser.add_argument(
         "--options",
         nargs="+",
@@ -48,15 +51,13 @@ vis_processor_cfg = cfg.datasets_cfg.cc_sbu_align.vis_processor.train
 vis_processor = registry.get_processor_class(vis_processor_cfg.name).from_config(vis_processor_cfg)
 chat = Chat(model, vis_processor, device='cuda:{}'.format(args.gpu_id))
 
-# annotations = json.load(open("/nas-alinlp/zcl/MiniGPT-4-main/dataset_coco.json", "r"))
-# annotations = annotations["images"]
-# image_path = "/nas-alinlp/yiyang.zyy/data_shop/coco/val2014"
+
 results = []
-#/iris/u/huaxiu/ch/mgpt-4/rewrite_input/llava_cocotest_des_sample_context_idnt1.jsonl
-input_dir = "/iris/u/huaxiu/ch/mgpt-4/coco5000_val/coco5000"
-output_file = "/iris/u/huaxiu/ch/mgpt-4/output_json/minigpt4_rewrite_nocoo.jsonl" 
+input_dir =  args.input_image
+output_file = args.output_file 
+input_caption = args.input_caption
 caption_data = []
-with open("/iris/u/huaxiu/ch/mgpt-4/mgpt4_coco5k_des_sample_context_idnt1.jsonl", 'r', encoding='utf-8') as f:
+with open(input_caption, 'r', encoding='utf-8') as f:
     for line in f:
         caption_data.append(json.loads(line.strip()))
 with torch.no_grad():
@@ -96,10 +97,10 @@ with torch.no_grad():
                 image = chat.vis_processor(image).unsqueeze(0).to('cuda:{}'.format(args.gpu_id))
                 image_emb, _, = chat.model.encode_img(image)
                 img_list.append(image_emb)
-                output, _,  _, u_wordlist, wordlist, plist, p_all = chat.answer(chat_state, img_list)
+                output = chat.answer(chat_state, img_list)
 
                 float_list = [tensor.item() for tensor in plist]
-                result = {"id": filename, "question": this_question, "caption": output,"uncertain_objs": u_wordlist, "plist": float_list, "p_all": p_all, "model": "MiniGPT-4_13b"}
+                result = {"id": filename, "question": this_question, "caption": output, "model": "LURE"}
                 json.dump(result, f)
                 f.write('\n')
                 f.flush()
